01802ecf96886a7a41d4de886d4078aae0798818
Alex <alexsheen@uchicago.edu>
2021-06-04 01:58:38 -0500

    granular synthesis

--- js/granular.js
+++ js/granular.js
@@ -3,50 +3,62 @@
 */
 const ctx = new (window.AudioContext || window.webkitAudioContext)()
 const fft = new AnalyserNode( ctx )
-const src = new AudioBufferSourceNode( ctx )
 const gn = new GainNode(ctx, {gain:0.5})
-
-src.connect(gn)
-gn.connect(fft)
-fft.connect(ctx.destination)
 
 createWaveCanvas({element:'section', analyser:fft })
 
-function fetchAudio(file) {
-  fetch(file)
-  .then(res => res.arrayBuffer() )
-  .then(data => {
-      // use the AudioContext's decodeAudioData method to decode
-      // the audio data contained in the buffer
-      ctx.decodeAudioData(data, (buffer)=>{
-          // set the BufferSourceNode's buffer to the decoded buffer
-          src.buffer = buffer
-          // now you can play the audio
-          src.start()
-      })
+let file
+let paused = false
+let start = 23.87
+let end = 0.5
+
+const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
+
+let buff
+fetch('billie_eilish_everything_i_wanted _studio_capella_trimmed.mp3')
+.then(res => res.arrayBuffer() )
+.then(data => {
+  ctx.decodeAudioData(data, (buffer) => {
+    buff = buffer
+    createBufferCanvas({
+        element:'section',
+        buffer:buff,
+        scale:0.5
+    })
+    updateBuff()
   })
-  .catch(err => { console.error(err) })
+})
+.catch(err => { console.error(err) })
+
+let src
+function updateBuff () {
+  src = new AudioBufferSourceNode( ctx, {
+      loop:true,
+      loopStart: start, // in-point offset
+      loopEnd: start + end // out-point offset
+  })
+
+  src.buffer = buff
+  src.start( ctx.currentTime, start )
+  src.connect(gn)
+  gn.connect(fft)
+  fft.connect(ctx.destination)
 }
-function changedVocal() {
-  let opt = document.getElementById("audio").value;
-  let file
-  console.log(opt)
-  switch(opt) {
-  case 'v1':
-    file = '/oversampled/Oversampled_vocal_chops_01_120_A'
-    fetchAudio(file)
-    break;
-  case 'v2':
-    file = '/oversampled/Oversampled_vocal_chops_03_120_A'
-    fetchAudio(file)
-    break;
-  case 'v3':
-    file = '/oversampled/Oversampled_vocal_chops_82_160_C'
-    fetchAudio(file)
-    break;
-  case 'v4':
-    file = '/oversampled/Oversampled_vocal_chops_138_180_Bm'
-    fetchAudio(file)
-    break;
+
+function position(val) {
+
+  src.stop()
+  start = map(val, 1,1000,1,src.buffer.duration)
+  updateBuff()
+  src.loopStart = map(val, 1,1000,1,src.buffer.duration)
+  src.loopEnd = Math.min(src.loopStart + end, src.buffer.duration)
+  src.loop = true
 }
+
+function length(val) {
+  src.stop()
+  updateBuff()
+  end = map(val, 1,1000,0,1)
+  src.loopEnd = src.loopStart + end
+  src.loop = true
 }
